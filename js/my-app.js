a6f02a0e526b7de635faf7945ff31a4461b1a4dd
Mac <michaelhertz@ssh-mac.home>
2015-11-24 13:36:03 -0500

    lunch

--- js/my-app.js
+++ js/my-app.js
@@ -144,7 +144,8 @@ myApp.onPageInit('setDisplay', function (page) {
 myApp.onPageInit('lights', function (page) {
 	var HostIP=storedData.wemoIP;
 	var HostPort=storedData.wemoPort;
-	var URL= 'http://' + HostIP + ':' + HostPort;
+	var DDNS=storedData.DDNSurl
+	var URL= 'http://' + DDNS + ':' + HostPort;
 	var postbodyheader= '<?xml version="1.0" encoding="utf-8"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body>';
 	var postbodyfooter= '</s:Body></s:Envelope>';
  
@@ -158,17 +159,12 @@ myApp.onPageInit('lights', function (page) {
 	var body = [
 	  postbodyheader, 
 	  '<u:GetEndDevices xmlns:u="urn:Belkin:service:bridge:1">', 
-<<<<<<< HEAD
-	  '<DevUDN>uuid:Bridge-1_0-231445B0100742</DevUDN>', 
-=======
-	  '<DevUDN>%s</DevUDN>', 
->>>>>>> origin/master
+	  '<DevUDN>' + storedData.wemoUDN + '</DevUDN>',
 	  '<ReqListType>PAIRED_LIST</ReqListType>',
 	  '</u:GetEndDevices>',
 	  postbodyfooter
 	].join('\n');
-<<<<<<< HEAD
-	
+	console.log(body);
 	var xmlhttp;
 	var cmd = '/upnp/control/bridge1';
 	xmlhttp = new XMLHttpRequest();
@@ -179,6 +175,7 @@ myApp.onPageInit('lights', function (page) {
 	//xmlhttp.setRequestHeader("User-Agent","QuickSwitch/507 CFNetwork/758.1.6 Darwin/15.0.0");
 	xmlhttp.send(body);
 	//var xmlDoc=xmlhttp.responseText;
+	
 	var xmlResponse =xmlhttp.responseXML.documentElement;
 	var resultSet = xmlhttp.responseXML.getElementsByTagName("*");
 	
@@ -186,59 +183,36 @@ myApp.onPageInit('lights', function (page) {
 	var xmlstr= resultSet[3].childNodes[0].nodeValue;
 	
 	var foo = jQuery(xmlstr).find('friendlyname');
-	for (i=0; i< foo.length; i++){
-	console.log(foo[i].innerHTML);
-	}
 	
-=======
-
-	var xmlhttp;
-	var cmd = '/upnp/control/bridge1';
-	xmlhttp = new XMLHttpRequest();
-	xmlhttp.open("POST", URL+cmd, true);
-	xmlhttp.setRequestHeader("Content-Type", "text/xml; charset=utf-8");
-	xmlhttp.setRequestHeader("SOAPAction", "urn:Belkin:service:bridge:1#GetEndDevices");
-	xmlhttp.setRequestHeader("Accept","");
-	xmlhttp.send(body);
-
-
->>>>>>> origin/master
-
-                     
+	for (i=0; i< foo.length; i++){
+		var newDiv = $$('#wemoControls').html();
+		$$('#wemoSpot').append(newDiv);
+		//$$('#wemoControls').addClass('dynamicWemoControl');
+		//alert(foo[i].innerHTML);
+			$$('#WemoFriendlyName').html(i);
+			$$('#roomSwitchData').attr('data-id', foo[i].innerText)
+			$$('#roomSwitchData').attr('id', foo[i].innerText+'_SwitchData');
+			$$('#roomSwitch').attr('id', foo[i].innerText+'_Switch');
+			$$('#roomSwitch').attr('data-id', foo[i].innerText)
+			
+			//$$('.WemoFriendlyName').attr('id', foo[i].innerText);
+		
+		
+		
+		
+		console.log( i + ":" + foo[i].innerHTML);
+	}
+	  
 	$$('#roomSwitch').on('click', function () {
 			
 	
-			var xmlhttp;
-			var cmd = '/upnp/control/basicevent1';
-			xmlhttp = new XMLHttpRequest();
-			xmlhttp.open("POST", URL+cmd, true);
-			xmlhttp.setRequestHeader("Content-Type", "text/xml; charset=utf-8");
-			xmlhttp.setRequestHeader("SOAPAction", "urn:Belkin:service:basicevent:1#SetBinaryState");
-			xmlhttp.setRequestHeader("Accept","");
-	
+			
 
 		if(!$$('#roomSwitchData').prop('checked')){
 			console.log('on');
-			var body = [
-				postbodyheader,
-				'<u:GetBinaryState xmlns:u="urn:Belkin:service:basicevent:1">',
-				'<BinaryState>1</BinaryState>',
-				'</u:GetBinaryState>',
-				postbodyfooter
-			].join('\n');    		
-			
-			xmlhttp.send(body);
 		}
 			else{
 			console.log('off');
-			var body = [
-				postbodyheader,
-				'<u:GetBinaryState xmlns:u="urn:Belkin:service:basicevent:1">',
-				'<BinaryState>0</BinaryState>',
-				'</u:GetBinaryState>',
-				postbodyfooter
-			].join('\n');    		
-			xmlhttp.send(body);
 			}
 		
 	});
@@ -277,7 +251,7 @@ function drawPhotons() {
 	var newDiv = $$('#PhotonDetail').html();
 
 	for (i = 0; i < photons.length; i++) {
-		$$('.PhotonList').append(newDiv);
+		
 		$$('#dynamicPhotonLink').text(photons[i].name);
 		$$('#dynamicPhotonLink').addClass('dynamicForm');
 		$$('#dynamicPhotonLink').attr('data-val', photons[i].name)
